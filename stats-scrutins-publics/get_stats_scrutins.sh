cb6905d1af4b9ce0a186e5a466de30588eecb1c4
RouxRC <b.ooghe@gmail.com>
2013-12-13 03:01:53 +0100

    style

--- stats-scrutins-publics/get_stats_scrutins.sh
+++ stats-scrutins-publics/get_stats_scrutins.sh
@@ -27,12 +27,11 @@ rat_s="$fl_s,"$((1000 * $sol / $tot - 10 * $fl_s))
 rat_p="$fl_p,"$((1000 * $pub / $tot - 10 * $fl_p))
 dat=$(date "+%d/%m/%Y")
 
-echo "# Ratio de scrutins publics et solennels à l'Assemblée nationale
+echo "### Ratio de scrutins publics et solennels à l'Assemblée nationale
 
-Depuis le début de la 14ème législature en juin 2012, $tot scrutins électroniques se sont déroulés.
-Parmi eux, seuls $sol scrutins solennels (soit $rat_s%) mentionnent le nom de chacun des députés votants.
-Les $pub autres scrutins ($rat_p%) dits « publics » ne mentionnent que la liste des rebelles de chaque groupe.
+Depuis le début de la 14ème législature en juin 2012, $tot scrutins électroniques se sont déroulés.<br/>
+Parmi eux, seuls $sol scrutins solennels (soit $rat_s%) mentionnent le nom de chacun des députés votants.<br/>
+Les $pub autres scrutins ($rat_p%) dits « publics » ne mentionnent que la liste des rebelles de chaque groupe.<br/>
 Pour en savoir plus, voir la [note de Regards Citoyens](http://www.regardscitoyens.org/documents/notes/20130110-RegardsCitoyens-AN-transparence-des-votes.pdf).
 
-sources : [Assemblée nationale](http://www.assemblee-nationale.fr/14/scrutins/)
-le $dat" > README.md
+sources : [Assemblée nationale](http://www.assemblee-nationale.fr/14/scrutins/) le $dat" > README.md
